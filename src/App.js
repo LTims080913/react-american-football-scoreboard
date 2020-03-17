f3703117907247ddf5f87c9575ee3ad9e74301b6
Latosha Tims <ltims080913@hotmail.com>
2020-03-16 23:41:10 -0700

    finished assignment

--- src/App.js
+++ src/App.js
@@ -1,39 +1,128 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+  const [scores, setScores] = useState(0);
+  const [downs, setDowns] = useState(1);
+  const [seconds, setSeconds] = useState(120);
+  const [minutes, setMinutes] = useState(1)
+  const [gameStart, setGameStart] = useState(true);
+  const homeTeam = 'Raiders';
+  const awayTeam = 'Cowboys';
+
+ 
+
+  function adjustTime() {
+    setSeconds(seconds - 5)
+  }
+
+  function determineWin() {
+    setGameStart(!gameStart)
+    if (homeScore > awayScore) {
+      return alert(`And the winner is....` + homeTeam)
+    } else {
+      alert(`And the winner is ` + awayTeam)
+    }
+  }
+  function newQuarter() {
+    setSeconds(seconds * 0 + 30);
+  }
+  function startGame() {
+    setGameStart(gameStart)
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (gameStart) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1)
+      }, 1000)
+    } 
+    
+    return () => clearInterval(interval)
+    }, [gameStart, seconds])
+  
+
+  function moveBall() {
+    if (downs < 4) {
+      setDowns(downs + 1)
+    } else {
+      setDowns(downs * 0)
+    }
+  }
+
+  function advanceGame() {
+    setScores(scores + 1)
+    adjustTime();
+    console.log("Scores per quarter: " + scores)
+    if (scores === 3) {
+      setScores(0);
+      newQuarter();
+      
+      if (quarter < 4) {   // if not fourth quarter
+         setQuarter(quarter + 1)
+      }
+      else{ 
+        console.log("game over")
+        setQuarter(quarter * 0);
+        setHomeScore(homeScore * 0);
+        setAwayScore(awayScore * 0);
+        setDowns(downs * 0);
+        setSeconds(seconds * 0 + 30);
+        determineWin();
+        
+        
+        
+
+      }
+    } 
+  }
+
+  const touchdownHome = () => {setHomeScore(homeScore + 7)
+    advanceGame();
+    moveBall()}
+  const fieldgoalHome = () => {setHomeScore(homeScore + 3) 
+    advanceGame()}
+  const fieldgoalAway = () => {setAwayScore(awayScore + 3) 
+    advanceGame()}
+  const touchdownAway = () => {setAwayScore(awayScore + 7) 
+    advanceGame();
+    moveBall()}
 
   return (
-    <div className="container">
+    <div className="container" onClick={startGame}>
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{homeTeam}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{seconds}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 className="away__name">{awayTeam}</h2>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} downs={downs} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={touchdownHome}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={fieldgoalHome}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={touchdownAway}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={fieldgoalAway}>Away Field Goal</button>
         </div>
       </section>
     </div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-const BottomRow = () => {
+
+
+
+const BottomRow = (props) => {
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{props.downs}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -18,7 +22,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{props.quarter}</div>
       </div>
     </div>
   );
